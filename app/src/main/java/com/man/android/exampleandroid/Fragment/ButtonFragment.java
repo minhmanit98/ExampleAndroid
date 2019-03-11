ea2eb86bfa5ba65cbbb8d4d2e6b94955ff5e5809
minhmanit98 <5751071024@st.utc2.edu.vn>
2019-03-11 16:55:51 +0700

    Initial commit

---
+++ app/src/main/java/com/man/android/exampleandroid/Fragment/ButtonFragment.java
@@ -0,0 +1,19 @@
+package com.man.android.exampleandroid.Fragment;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.man.android.exampleandroid.R;
+
+public class ButtonFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.buttonfragment, container, false);
+    }
+}
---
+++ app/src/main/java/com/man/android/exampleandroid/MainActivity.java
@@ -0,0 +1,87 @@
+package com.man.android.exampleandroid;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.man.android.exampleandroid.Fragment.ButtonFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListViewMain listViewMain = new ListViewMain();
+
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button=(Button) findViewById(R.id.button);
+        initFragment(100);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                replaceFragmentContent(listViewMain);
+            }
+        });
+
+
+    }
+
+    public void initFragment(int selectfr) {
+
+
+        Fragment fragment = new Fragment();
+
+        ButtonFragment buttonFragment = new ButtonFragment();
+
+        switch (selectfr){
+            case 0: fragment=buttonFragment; break;
+            case 1: fragment=buttonFragment; break;
+            case 100:fragment=listViewMain; break;
+            default: fragment=listViewMain; break;
+        }
+
+
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        FragmentTransaction ft = fragmentManager.beginTransaction();
+
+        ft.replace(R.id.container_body,fragment );
+
+        ft.commit();
+
+    }
+
+
+
+    protected void replaceFragmentContent(Fragment fragment) {
+
+        if (fragment != null) {
+
+            FragmentManager fmgr = getSupportFragmentManager();
+
+            FragmentTransaction ft = fmgr.beginTransaction();
+
+            ft.replace(R.id.container_body, fragment);
+
+            ft.commit();
+
+        }
+
+    }
+
+
+
+
+}
