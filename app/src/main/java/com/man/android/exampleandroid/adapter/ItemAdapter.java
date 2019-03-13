3fcefec52723590746bc51b6350557904c41b141
minhmanit98 <5751071024@st.utc2.edu.vn>
2019-03-13 20:49:08 +0700

    Them icon cho listview

---
+++ app/src/main/java/com/man/android/exampleandroid/adapter/ItemAdapter.java
@@ -0,0 +1,53 @@
+package com.man.android.exampleandroid.adapter;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.mohammad.listviewexample.R;
+
+import java.util.ArrayList;
+
+public class ItemAdapter extends BaseAdapter {
+
+    private Context context;
+    private ArrayList<Integer> ids;
+    private ArrayList<String> names;
+    public ItemAdapter(Context context, ArrayList<Integer> ids, ArrayList<String> names) {
+        this.context = context;
+        this.ids=ids;
+        this.names = names;
+    }
+
+    @Override
+    public int getCount() {
+        return names.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return names.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        if (convertView == null) {
+            convertView = View.inflate(context, R.layout.list_items, null);
+        }
+        ImageView img = (ImageView) convertView.findViewById(R.id.imageView);
+        TextView tv = (TextView) convertView.findViewById(R.id.textView);
+        img.setImageResource(ids.get(position));
+        tv.setText(names.get(position));
+
+        return convertView;
+    }
+}
---
+++ app/src/main/java/com/man/android/exampleandroid/model/Models.java
@@ -0,0 +1,27 @@
+package com.man.android.exampleandroid.model;
+
+/**
+ * Created by mohammad on 3/21/2017.
+ */
+
+public class Models {
+
+    private int imageId;
+    private String imageName;
+
+    public int getImageId() {
+        return imageId;
+    }
+
+    public void setImageId(int imageId) {
+        this.imageId = imageId;
+    }
+
+    public String getImageName() {
+        return imageName;
+    }
+
+    public void setImageName(String imageName) {
+        this.imageName = imageName;
+    }
+}
